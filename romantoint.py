0eaf28a49dbd52b5185497fb9c5de64db0b83e07
teltorob <teltorob@gmail.com>
2021-08-21 21:57:19 +0530

    RomanToInt added

---
+++ romantoint.py
@@ -0,0 +1,27 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_conversion={
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000,
+        }
+        prevVal=0
+        total=0
+        
+        for char in s[::-1]:
+            val = roman_conversion[char]
+            if prevVal>val:
+                val =-val
+                
+            total+=val
+            prevVal=val
+        
+        return total
+
+if __name__ == "__main__":
+    soln=Solution()
+    print(soln.romanToInt("MCMXCIV"))
