f28d063333a7ddb896788b83ff25d3cb33c40019
Ayush Tiwari <teltorob@gmail.com>
2021-12-14 23:51:44 +0530

    Day 3 #100daysofcode

---
+++ binaryTreePaths.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        paths = []
+        def dfs(node, p):
+            if not node: return
+            
+            p.append(str(node.val))
+
+            if not node.left and not node.right:
+                paths.append('->'.join(p))
+                        
+            dfs(node.left,  p)
+            dfs(node.right, p)
+            
+            p.pop()
+            
+        dfs(root, [])
+        return paths
