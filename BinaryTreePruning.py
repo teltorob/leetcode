8eb239da2301cad7a697422a34f54c01482359e7
Ayush Tiwari <teltorob@gmail.com>
2022-06-12 22:14:05 +0530

    add dfs implementation of Binary Tree Pruning

---
+++ BinaryTreePruning.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        
+        def helper(root):
+            if not root:
+                return None
+            
+            root.left = helper(root.left)
+            root.right = helper(root.right)
+            
+            if not root.left and not root.right and root.val == 0:
+                return None
+            
+            return root
+        
+        return helper(root)
