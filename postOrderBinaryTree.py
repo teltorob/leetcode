9c906f55c9dc2f0ac3092f692b2a9fce28354c53
Ayush Tiwari <teltorob@gmail.com>
2022-06-14 00:01:25 +0530

    added Post order binary tree using dfs

---
+++ postOrderBinaryTree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.  ✔ 
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        def helper(root, arr):
+
+            if not root: return None
+
+            helper(root.left, arr)
+            helper(root.right, arr)
+            arr.append(root.val)
+
+            return arr
